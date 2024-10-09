9b2496a126082d3dcf5097919b4e8a79222d7b80
bllamond <vinaymalik1729@gmail.com>
2024-10-09 14:54:50 +0530

    route

--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -1,48 +1,48 @@
-import { v2 as cloudinary } from 'cloudinary';
-import { NextResponse } from 'next/server';
-import formidable from 'formidable';
+// import { v2 as cloudinary } from 'cloudinary';
+// import { NextResponse } from 'next/server';
+// import formidable from 'formidable';
 
 
-cloudinary.config({
-  cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
-  api_secret: process.env.NEXT_PUBLIC_CLOUDINARY_API_SECRET,
-});
+// cloudinary.config({
+//   cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+//   api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
+//   api_secret: process.env.NEXT_PUBLIC_CLOUDINARY_API_SECRET,
+// });
 
-export const config = {
-  api: {
-    bodyParser: false, 
-  },
-};
+// export const config = {
+//   api: {
+//     bodyParser: false, 
+//   },
+// };
 
-export async function POST(req) {
-  return new Promise((resolve, reject) => {
-    const form = new formidable.IncomingForm();
+// export async function POST(req) {
+//   return new Promise((resolve, reject) => {
+//     const form = new formidable.IncomingForm();
 
-    form.parse(req, async (err, fields, files) => {
-      if (err) {
-        console.error('Error parsing form data:', err);
-        return reject(
-          NextResponse.json({ error: 'Error parsing the file' }, { status: 500 })
-        );
-      }
+//     form.parse(req, async (err, fields, files) => {
+//       if (err) {
+//         console.error('Error parsing form data:', err);
+//         return reject(
+//           NextResponse.json({ error: 'Error parsing the file' }, { status: 500 })
+//         );
+//       }
 
-      try {
-        const file = files.file;
+//       try {
+//         const file = files.file;
 
-        const uploadResult = await cloudinary.uploader.upload(file.filepath, {
-          folder: 'uploads',
-          resource_type: 'image',
-        });
+//         const uploadResult = await cloudinary.uploader.upload(file.filepath, {
+//           folder: 'uploads',
+//           resource_type: 'image',
+//         });
 
-        resolve(
-          NextResponse.json({ url: uploadResult.secure_url }, { status: 200 })
-        );
-      } catch (uploadError) {
-        resolve(
-          NextResponse.json({ error: 'Cloudinary upload failed' }, { status: 500 })
-        );
-      }
-    });
-  });
-}
+//         resolve(
+//           NextResponse.json({ url: uploadResult.secure_url }, { status: 200 })
+//         );
+//       } catch (uploadError) {
+//         resolve(
+//           NextResponse.json({ error: 'Cloudinary upload failed' }, { status: 500 })
+//         );
+//       }
+//     });
+//   });
+// }
