9fd4f35a45d8a3f9e24a736e486a78c536dd80d2
bllamond <vinaymalik1729@gmail.com>
2024-10-09 14:50:47 +0530

    deprecated configuratio change

--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -2,6 +2,7 @@ import { v2 as cloudinary } from 'cloudinary';
 import { NextResponse } from 'next/server';
 import formidable from 'formidable';
 
+
 cloudinary.config({
   cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
   api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
@@ -10,21 +11,21 @@ cloudinary.config({
 
 export const config = {
   api: {
-    bodyParser: false,
+    bodyParser: false, 
   },
 };
 
 export async function POST(req) {
-    return new Promise((resolve, reject) => {
-        const form = new formidable.IncomingForm();
-    
-        form.parse(req, async (err, fields, files) => {
-          if (err) {
-            console.error('Error parsing form data:', err);
-            return reject(
-              NextResponse.json({ error: 'Error parsing the file' }, { status: 500 })
-            );
-          }
+  return new Promise((resolve, reject) => {
+    const form = new formidable.IncomingForm();
+
+    form.parse(req, async (err, fields, files) => {
+      if (err) {
+        console.error('Error parsing form data:', err);
+        return reject(
+          NextResponse.json({ error: 'Error parsing the file' }, { status: 500 })
+        );
+      }
 
       try {
         const file = files.file;
